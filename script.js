f43107f01177f8847390d566f85e8f8df971df60
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-07-12 13:40:03 -0300

    Renomeando

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-let player1Time;
-let player2Time;
-let interval;
-let currentPlayer = 1;
-const botaoBlack = document.getElementById('botao__player-black');
-const botaoWhite = document.getElementById('botao__player-white');
+let jogador1;
+let jogador2;
+let intervalo;
+let vezJogador = 1;
+const botaoPreto = document.getElementById('botao__player-black');
+const botaoBranco = document.getElementById('botao__player-white');
 const iniciar = document.getElementById('start-btn');
 const reiniciar = document.getElementById('restart-btn');
-const whitePiece = document.getElementById('white-piece');
-const blackPiece = document.getElementById('black-piece');
-const timePlayer1 = document.getElementById('player1-time')
-const timePlayer2 = document.getElementById('player2-time')
+const pecaBranca = document.getElementById('white-piece');
+const pecaPreta = document.getElementById('black-piece');
+const tempoJogador1 = document.getElementById('player1-time')
+const tempoJogador2 = document.getElementById('player2-time')
 
-botaoBlack.addEventListener('click', () => {
-    animatePiece()
-    startClock
-    switchPlayer()
+botaoPreto.addEventListener('click', () => {
+    mostrarPeca()
+    iniciarRelogio()
+    selecionaVezJogador()
     console.log('Cliquei');
-    botaoBlack.setAttribute('disabled', true)
-    botaoWhite.removeAttribute('disabled')
-    console.log(currentPlayer);
+    botaoPreto.setAttribute('disabled', true)
+    botaoBranco.removeAttribute('disabled')
 })
-botaoWhite.addEventListener('click', () => {
-    animatePiece()
-    startClock()
-    switchPlayer()
+botaoBranco.addEventListener('click', () => {
+    mostrarPeca()
+    iniciarRelogio()
+    selecionaVezJogador()
     console.log('Cliquei');
-    botaoWhite.setAttribute('disabled', true)
-    botaoBlack.removeAttribute('disabled')
-    console.log(currentPlayer);
+    botaoBranco.setAttribute('disabled', true)
+    botaoPreto.removeAttribute('disabled')
 })
 
 function updateClock(player, time) {
@@ -37,75 +35,73 @@ function updateClock(player, time) {
     document.getElementById(`${player}-time`).textContent = formattedTime;
 }
 
-function switchPlayer() {
-    clearInterval(interval);
-    currentPlayer = currentPlayer === 1 ? 2 : 1;
+function selecionaVezJogador() {
+    clearInterval(intervalo);
+    vezJogador = vezJogador === 1 ? 2 : 1;
 }
 
-function startClock() {
-    interval = setInterval(() => {
-        if (currentPlayer === 1) {
-            player1Time--;
-            updateClock('player1', player1Time);
-            if (timePlayer1 < 11) {
+function iniciarRelogio() {
+    intervalo = setInterval(() => {
+        if (vezJogador === 1) {
+            jogador1--;
+            updateClock('player1', jogador1);
+            if (tempoJogador1 < 11) {
                 clearInterval(interval)
                 alert('lsdkjj')
-                timePlayer1.style.backgroundColor = "#FF0F0F";
+                tempoJogador1.style.backgroundColor = "#FF0F0F";
             }
         } else {
-            player2Time--;
-            updateClock('player2', player2Time);
-            if (player2Time <= 0) {
-                clearInterval(interval);
+            jogador2--;
+            updateClock('player2', jogador2);
+            if (jogador2 <= 0) {
+                clearInterval(intervalo);
                 alert('Jogador 2 perdeu!');
             }
         }
     }, 1000);
 }
 
-function animatePiece() {
-    if (currentPlayer === 1) {
-        whitePiece.style.opacity = '0';
-        blackPiece.style.opacity = '1';
+function mostrarPeca() {
+    if (vezJogador === 1) {
+        pecaBranca.style.opacity = '0';
+        pecaPreta.style.opacity = '1';
     } else {
-        whitePiece.style.opacity = '1';
-        blackPiece.style.opacity = '0';
+        pecaBranca.style.opacity = '1';
+        pecaPreta.style.opacity = '0';
     }
 }
 
 iniciar.addEventListener('click', () => {
-    const selectedTime = parseInt(document.getElementById('time-select').value) / 2;
-    player1Time = selectedTime;
-    player2Time = selectedTime;
-    currentPlayer = 1; // Sempre começa com as peças brancas
-    updateClock('player1', player1Time);
-    updateClock('player2', player2Time);
-    whitePiece.style.opacity = '1';
-    startClock();
-    botaoWhite.removeAttribute('disabled')
+    const selecionaTempo = parseInt(document.getElementById('time-select').value) / 2;
+    jogador1 = selecionaTempo;
+    jogador2 = selecionaTempo;
+    vezJogador = 1; // Sempre começa com as peças brancas
+    updateClock('player1', jogador1);
+    updateClock('player2', jogador2);
+    iniciarRelogio();
+    pecaBranca.style.opacity = '1';
+    botaoBranco.removeAttribute('disabled')
     iniciar.setAttribute('disabled', true)
     reiniciar.removeAttribute('disabled')
 });
 
 // modal reiniciar
 const modal = document.getElementById("myModal");
-const reiniciarModal = document.getElementById('restart-btn-modal')
-const fecharModal = document.getElementById('cancel-btn-modal')
+const sim = document.getElementById('restart-btn-modal')
+const nao = document.getElementById('cancel-btn-modal')
 const fechar = document.getElementById("close__modal");
 
-reiniciarModal.addEventListener('click', () => {
+sim.addEventListener('click', () => {
     modal.style.display = "none";
     window.location.reload();
-    
 })
-fecharModal.addEventListener('click', () => {
+nao.addEventListener('click', () => {
     modal.style.display = "none";
 })
 
 reiniciar.addEventListener('click', () => {
     modal.style.display = "block";
 })
-
 
 fechar.addEventListener('click', () => {
     modal.style.display = "none";
