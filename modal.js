be5fb2fadc931e84d789fce63062043128f156d9
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-07-13 10:07:27 -0300

    Criando modal.js
    Separando a lógica do modal em um modulo js

---
+++ modal.js
@@ -0,0 +1,30 @@
+const modal = document.getElementById("myModal");
+
+export function iniciarModal(){
+  const sim = document.getElementById('restart-btn-modal')
+  const nao = document.getElementById('cancel-btn-modal')
+  const fechar = document.getElementById("close__modal");
+  
+  sim.addEventListener('click', () => {
+      modal.style.display = "none";
+      window.location.reload()
+  })
+  nao.addEventListener('click', () => {
+      modal.style.display = "none";
+  })
+   
+  fechar.addEventListener('click', () => {
+      modal.style.display = "none";
+  })
+  
+  window.addEventListener('click', () => {
+      if (event.target == modal) {
+          modal.style.display = "none";
+      }
+  })
+
+}
+
+export function mostrarModal(){
+  modal.style.display = 'block'
+}
