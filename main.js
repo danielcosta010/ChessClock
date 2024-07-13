a40838544904a1ab40760fc23de47eda11c8b832
Daniel Costa <105571748+danielcosta010@users.noreply.github.com>
2024-07-13 10:08:07 -0300

    Renomeando script.js e chamando modal

---
+++ main.js
@@ -0,0 +1,138 @@
+import { iniciarModal, mostrarModal } from "./modal.js";
+
+let jogador1;
+let jogador2;
+let intervalo = 0;
+let vezJogador = 1;
+const botaoPreto = document.getElementById('botao__player-black');
+const botaoBranco = document.getElementById('botao__player-white');
+const iniciar = document.getElementById('start-btn');
+const reiniciar = document.getElementById('restart-btn');
+const pecaBranca = document.getElementById('white-piece');
+const pecaPreta = document.getElementById('black-piece');
+const tempoJogador1 = document.getElementById('player1-time')
+const tempoJogador2 = document.getElementById('player2-time')
+
+botaoPreto.addEventListener('click', () => {
+    mostrarPeca()
+    iniciarRelogio()
+    selecionaVezJogador()
+    botaoPreto.setAttribute('disabled', true)
+    botaoBranco.removeAttribute('disabled')
+})
+botaoBranco.addEventListener('click', () => {
+    mostrarPeca()
+    iniciarRelogio()
+    selecionaVezJogador()
+    botaoBranco.setAttribute('disabled', true)
+    botaoPreto.removeAttribute('disabled')
+})
+
+function atualizaRelogio(player, time) {
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+    const formattedTime = `${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
+    document.getElementById(`${player}-time`).textContent = formattedTime;
+}
+
+function selecionaVezJogador() {
+    clearInterval(intervalo);
+    vezJogador = vezJogador === 1 ? 2 : 1;
+}
+
+function iniciarRelogio() {
+    intervalo = setInterval(() => {
+        if (vezJogador === 1) {
+            jogador1--;
+            atualizaRelogio('player1', jogador1);
+            if (jogador1 <= 0) {
+                temposEsgotado(tempoJogador1)
+            }
+        } else {
+            jogador2--;
+            atualizaRelogio('player2', jogador2);
+            if (jogador2 <= 0) {
+                temposEsgotado(tempoJogador2)
+            }
+        }
+    }, 1000);
+}
+
+function temposEsgotado(tempoJogador) {
+    clearInterval(intervalo)
+    tempoJogador.innerHTML = 'Tempo Esgotado!'
+    tempoJogador.style.color = 'red'
+    botaoBranco.setAttribute('disabled', true)
+    botaoPreto.setAttribute('disabled', true)
+}
+
+function mostrarPeca() {
+    if (vezJogador === 1) {
+        pecaBranca.style.opacity = '0';
+        pecaPreta.style.opacity = '1';
+        pecaBranca.play()
+    } else {
+        pecaBranca.style.opacity = '1';
+        pecaPreta.style.opacity = '0';
+        pecaPreta.play()
+    }
+}
+
+iniciar.addEventListener('click', () => {
+    const selecionaTempo = parseInt(document.getElementById('time-select').value) / 2;
+    jogador1 = selecionaTempo;
+    jogador2 = selecionaTempo;
+    vezJogador = 1;
+    atualizaRelogio('player1', jogador1);
+    atualizaRelogio('player2', jogador2);
+    iniciarRelogio();
+    requestWakeLock()
+    pecaBranca.style.opacity = '1';
+    botaoBranco.removeAttribute('disabled')
+    iniciar.setAttribute('disabled', true)
+    reiniciar.removeAttribute('disabled')
+});
+
+iniciarModal()
+
+reiniciar.addEventListener('click', () => {
+    mostrarModal()
+})
+
+
+
+let wakeLock = null;
+async function requestWakeLock() {
+    try {
+        wakeLock = await navigator.wakeLock.request('screen');
+        console.log('Wake Lock ativado');
+        wakeLock.addEventListener('release', () => {
+            console.log('Wake Lock desativado');
+        });
+    } catch (err) {
+        console.error(`${err.name}, ${err.message}`);
+    }
+}
+
+function releaseWakeLock() {
+    if (wakeLock) {
+        wakeLock.release()
+            .then(() => {
+                wakeLock = null;
+                console.log('Wake Lock liberado');
+            })
+            .catch(err => {
+                console.error(`${err.name}, ${err.message}`);
+            });
+    }
+}
+
+// Pausar os vídeos quando a página é descarregada
+window.addEventListener('beforeunload', () => {
+    whitePieceVideo.pause();
+    blackPieceVideo.pause();
+    releaseWakeLock(); // Liberar Wake Lock
+});
+
+// // Libera o bloqueio de tela quando a página é descarregada
+// window.addEventListener('beforeunload', liberarTela);
